789dcd183f51c743726179016f1ca716532b0cb6
razam <razam@ubuntu.ubuntu-domain>
2019-07-30 00:37:01 +0300

    added get mapping

--- src/main/java/com/techpremises/aws/awselasticbeanstalkexample2/HelloController.java
+++ src/main/java/com/techpremises/aws/awselasticbeanstalkexample2/HelloController.java
@@ -1,11 +1,13 @@
 package com.techpremises.aws.awselasticbeanstalkexample2;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
+    @GetMapping
     public String hello()
     {
         return "hello youtube";
