270ccdccd2c0a42133cbc754bce6387db2b3210a
razam <razam@ubuntu.ubuntu-domain>
2019-07-29 21:20:11 +0300

    Beanstalk example

---
+++ src/main/java/com/techpremises/aws/awselasticbeanstalkexample2/HelloController.java
@@ -0,0 +1,13 @@
+package com.techpremises.aws.awselasticbeanstalkexample2;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+    public String hello()
+    {
+        return "hello youtube";
+    }
+}
